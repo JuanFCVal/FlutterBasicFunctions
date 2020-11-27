b616bc8313c627a78771e8e7770ea968acd64da5
Juan Francisco Cevallos <cevallosjuanfrancisco@gmail.com>
2020-11-27 15:35:02 -0500

    Setting up proyect and start to work with ListView

---
+++ examples/lib/src/pages/home_temp.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class HomePageTemp extends StatelessWidget {
+  HomePageTemp({Key key}) : super(key: key);
+  final op = ["Uno", "Dos", "tRES"];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Titulo"),
+      ),
+      body: ListView(
+        children: _crearItemsConFor(),
+      ),
+    );
+  }
+
+  List<Widget> _crearItemsConFor() {
+    //Primer modo
+    List<Widget> lista = new List<Widget>();
+    for (var x in op) {
+      final tempWidget = ListTile(
+        title: Text(x),
+      );
+      //lista.add(tempWidget);
+      //lista.add(Divider());
+      lista
+        ..add(tempWidget)
+        ..add(
+            Divider()); //Esto hace lo mismo que las dos lineas comentadas antes
+    }
+    return lista;
+  }
+
+  List<Widget> _crearItemsConMap() {
+    return op
+        .map((e) => Column(
+              children: <Widget>[
+                ListTile(
+                  title: Text(e + "!"),
+                  leading: Icon(Icons
+                      .access_alarm), //Se escribe al inicio del objeto de la lista
+                  trailing: Icon(Icons
+                      .arrow_back), //Se escribe al final del objeto de la lista
+                  onTap: () => null,
+                ),
+                Divider()
+              ],
+            ))
+        .toList();
+  }
+}
